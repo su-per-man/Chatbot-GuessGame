0e2fc6ce19866b936d753b818d083badd919ffc0
Suman Kumar <suman98kumar@gmail.com>
2020-10-04 22:48:13 +0530

    First working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, InputBase, Container, AppBar, Toolbar, Typography, IconButton, Icon } from '@material-ui/core'
+import ChatMsg from './chatMsg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const rand = Math.ceil(1 + Math.random() * 99)
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      chat: [<ChatMsg avatar={''} messages={['Hey There!',
+        'Myself Natasha ',
+        'Let\'s play a game',
+        'Guess a number between 1 and 100']} />],
+      userGuess: '',
+      attempt: 0
+    }
+  }
+  handleGuess = () => {
+    this.setState({
+      chat:
+        [...this.state.chat, <ChatMsg side={'right'} messages={[this.state.userGuess]} />],
+      attempt: this.state.attempt + 1,
+    }, () => {
+      let temp = null
+      let guess = parseInt(this.state.userGuess)
+      switch (true) {
+        case (guess > rand):
+          temp = <ChatMsg avatar={''} messages={['Try smaller number']} />
+          break;
+        case (guess < rand):
+          temp = <ChatMsg avatar={''} messages={['Try bigger number']} />
+          break;
+        case (guess === rand):
+          temp = <ChatMsg avatar={''} messages={['That\'s correct!!', 'You have taken only ' + this.state.attempt + ' attempts to crack it']} />
+          break;
+        default:
+          temp = <ChatMsg avatar={''} messages={['Oops! Please enter a number']} />
+      }
+      this.setState({
+        chat: [...this.state.chat, temp],
+        userGuess: ''
+      })
+    })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Container maxWidth="xs">
+          <AppBar elevation={0} position="static" color="primary" >
+            <Toolbar variant="dense">
+              <Typography>Hack Natasha's Mind</Typography>
+            </Toolbar>
+          </AppBar>
+          <Box my={3}>
+            {this.state.chat}
+          </Box>
+
+          <Box py={1} style={{ display: "flex" }}>
+            <InputBase placeholder="Guess a number" style={{ flex: 2 }} value={this.state.userGuess}
+              onChange={(e) => this.setState({
+                userGuess: e.target.value
+              })} autoFocus />
+            <IconButton color="primary" onClick={this.handleGuess}>
+              <Icon>send</Icon>
+            </IconButton>
+          </Box>
+        </Container>
+      </React.Fragment >
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#09f" // This is an orange looking color
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
